0f92685942bedc18adbc9322fcf8fd9a18593bde
goodobearo <82933015+goodobearo@users.noreply.github.com>
2023-02-02 01:33:31 -0500

    Fixed a typo in the name inside the registerBlock function.

--- src/main/java/com/goodobearo/mobexpansionmod/block/ModBlocks.java
+++ src/main/java/com/goodobearo/mobexpansionmod/block/ModBlocks.java
@@ -21,22 +21,27 @@ import java.util.function.Supplier;
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MobExpansionMod.MOD_ID);
 
-    public static final RegistryObject<Block> CRUDE_RED_CRYSTAL_CLUSTER = registerBlock( "crude_red_crysal_cluster",
+    public static final RegistryObject<Block> CRUDE_RED_CRYSTAL_CLUSTER = registerBlock( "crude_red_crystal_cluster",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(6f).requiresCorrectToolForDrops(),
                     UniformInt.of(1, 3)), ModCreativeModeTab.MOB_EXPANSION_MOD_TAB_2);
 
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
+
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn, tab);
         return toReturn;
+
     }
 
     private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab) {
+
         return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+
     }
 
     public static void register(IEventBus eventBus) {
 
         BLOCKS.register(eventBus);
+
     }
 }
